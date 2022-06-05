3db0f46b538057eb462dfdce6d9bc59fd894ba43
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-05 20:25:00 +0200

    setup Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { MainMusicPanelComponent } from './components/main-music-panel/main-music-panel.component';
+import { MainMusicPlaylistsComponent } from './components/main-music-playlists/main-music-playlists.component';
+import { MainPageComponent } from './view/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavbarComponent,
+    MainMusicPanelComponent,
+    MainMusicPlaylistsComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
