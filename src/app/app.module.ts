d449294bc74a290af1c46f38252a405ec1d32cb0
Nathanael421 <nathanael.rayapin@gmail.com>
2022-06-06 22:34:21 +0200

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
-import { MainMusicPanelComponent } from './components/main-music-panel/main-music-panel.component';
-import { MainMusicPlaylistsComponent } from './components/main-music-playlists/main-music-playlists.component';
+import { MainVideoPanelComponent } from './components/main-video-panel/main-video-panel.component';
+import { MainVideoPlaylistsComponent } from './components/main-video-playlists/main-video-playlists.component';
 import { MainPageComponent } from './view/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavbarComponent,
-    MainMusicPanelComponent,
-    MainMusicPlaylistsComponent,
+    MainVideoPanelComponent,
+    MainVideoPlaylistsComponent,
     MainPageComponent
   ],
   imports: [
